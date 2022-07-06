081b08b479c4ce32ef48e7bd0ff48a5d64ef5e84
LucasCamposss <94052022+LucasCamposss@users.noreply.github.com>
2022-07-06 16:32:54 -0300

    06/07

--- Codigo_Malha_Aberta/Codigo_Malha_Aberta.ino
+++ Codigo_Malha_Aberta/Codigo_Malha_Aberta.ino
@@ -1,46 +1,42 @@
 const int pot = 34;
 const int motor1 = 25;
 const int motor2 = 33;
-const int motorLeitura = 35;
+const int motorLeitura = 26;
 int leitura;
 float tensao;
 
 
-const int freq = 20000;
+const int freq = 30000;
 const int ledChannel = 0;
 const int resolution = 12;
 
 void setup() {
   Serial.begin(115200);
   pinMode(pot, INPUT);
-  pinMode(motorLeitura,INPUT);
+  pinMode(motorLeitura, INPUT);
   pinMode(motor1, OUTPUT);
   pinMode(motor2, OUTPUT);
-  
-  ledcSetup(ledChannel, freq, resolution); 
+
+  ledcSetup(ledChannel, freq, resolution);
   ledcAttachPin(motor1, ledChannel);
 }
-  
+
 void loop() {
   leitura = analogRead(pot);
   tensao = float(analogRead(motorLeitura));
-  tensao = 2*tensao/1241;
-  Serial.println(tensao);
+  tensao = 2 * tensao / 1240.909;
+//  Serial.println(tensao);
   motorPWM(leitura);
-  
-  
 }
 void motorPWM (int valor)
 {
-    if(valor > 1000){
-      valor = 4095*0.7;
-    }
- 
-    if(valor <= 1000){
-      valor = 4095*0;
-    }
-    
- 
-    ledcWrite(ledChannel, valor);
-    digitalWrite(motor2,LOW); 
+  //    if(valor > 1000){
+  //      valor = 4095*0.7;
+  //    }
+  //
+  //    if(valor <= 1000){
+  //      valor = 4095*0;
+  //    }
+  ledcWrite(ledChannel, valor);
+  digitalWrite(motor2, LOW);
 }
--- Controle_PID/Controle_PID.ino
+++ Controle_PID/Controle_PID.ino
@@ -1,9 +1,12 @@
+#include "esp_adc_cal.h"
+
 const int pot = 34;
 const int motor1 = 25;
 const int motor2 = 33;
-const int motorLeitura = 35;
+const int motorLeitura = 26;
 int leitura;
 float tensao;
+float entrada;
 
 float alvo;
 float erro;
@@ -28,30 +31,51 @@ void setup() {
   ledcSetup(ledChannel, freq, resolution); 
   ledcAttachPin(motor1, ledChannel);
 
-  alvo = 2000;
-  kp = 1;
-  ki = 0;
-  kd = 0;
+  alvo = 3.0;
+  kp = 0.001;
+  ki = 0.00;
+  kd = 0.00;
   valorPID = 0;
   erro0 = 0;
   sumErro = 0;
+  entrada = 0;
 }
   
 void loop() {
+  if (Serial.available() > 0) {
+    // lê do buffer o dado recebido:
+    alvo = (Serial.readString()).toFloat();
+  }
+  if (alvo<=0){
+    alvo = 0.00001;
+  }
   tensao = float(analogRead(motorLeitura));
-  erro = alvo - tensao;
+  tensao = 2*readADC_Cal(tensao)*0.001;
+  
+  erro = (alvo - tensao)/alvo;
   ajustaPID(erro, &valorPID, &sumErro, &erro0, kp, ki, kd);
   
-//  Serial.println(tensao);
-  motorPWM(valorPID); 
+  entrada = entrada+valorPID;
+  if(entrada>1){
+    entrada = 1;
+  }
+  if(entrada<0){
+    entrada = 0;
+  }
+  motorPWM(entrada); 
 }
 
 
-void motorPWM (int valor)
+void motorPWM (float valor)
 {   
-    if(valor>4095){
-      valor = 4095;
-      }
+    if(valor>1){
+      valor = 1;
+    }
+    if(valor<0){
+      valor = 0;
+    }
+    valor = valor*4095;
+         
     ledcWrite(ledChannel, valor);
     digitalWrite(motor2,LOW); 
 }
@@ -62,4 +86,13 @@ void ajustaPID (float erro,float *valorPID,float *sumErro,float *erro0,float kp,
     *valorPID = erro*kp + *sumErro*ki + (erro - *erro0)*kd;
     *sumErro += erro;
     *erro0 = erro;
+}
+
+
+uint32_t readADC_Cal(int ADC_Raw)
+{
+  esp_adc_cal_characteristics_t adc_chars;
+  
+  esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1000, &adc_chars);
+  return(esp_adc_cal_raw_to_voltage(ADC_Raw, &adc_chars));
 }
